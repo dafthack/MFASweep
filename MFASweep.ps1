c34fdfda132c75839b5936e582b8f5bf9e03fd5d
Bryan McNulty <bryanmcnulty@protonmail.com>
2024-10-03 01:39:27 -0500

    Add support for Linux filesystem paths in Write-CookiesToFile & Write-TokensToFile

--- MFASweep.ps1
+++ MFASweep.ps1
@@ -1077,7 +1077,7 @@ Function Write-TokensToFile {
     )
 
     if ($WriteTokens) {
-        $tokenFilePath = "$PSScriptRoot\AccessTokens.json"
+        $tokenFilePath = Join-Path ("$PSScriptRoot" ? "$PSScriptRoot" : ".") "AccessTokens.json"
         $currentDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
         # Create the new token entry
@@ -1115,7 +1115,7 @@ Function Write-CookiesToFile {
         [string]$UserAgent
     )
 
-    $tokenFilePath = "$PSScriptRoot\AccessTokens.json"
+    $tokenFilePath = Join-Path ("$PSScriptRoot" ? "$PSScriptRoot" : ".") "AccessTokens.json"
     $currentDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
     # Prepare cookie data
